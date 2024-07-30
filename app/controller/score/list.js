ba45bded9e87cd798a3be3ee4c1eed4536ea8586
Souta <soutacn@gmail.com>
2024-07-30 21:57:53 +0800

    feat: 成绩列表新增Redis缓存

--- app/controller/score/list.js
+++ app/controller/score/list.js
@@ -1,6 +1,7 @@
 // app/controller/score/list.js
 
 const { Controller } = require('egg');
+const dayjs = require('dayjs');
 
 // 定义创建接口的请求参数规则
 const createRule = {
@@ -19,75 +20,126 @@ class ScoreListController extends Controller {
     const term = ctx.query.term;
     // 初始化个人信息
     const user = ctx.user_info;
-    // 获取登录信息
-    const pass = await ctx.app.mysql.select('user', {
-      where: {
-        student_id: user.student_id
-      }
-    });
-
-    try {
-      // 成绩获取地址
-      const score_base_url = ctx.app.config.jwxt.base + ctx.app.config.jwxt.score;
-      const score_url = `${score_base_url}?page.size=300&page.pn=1&startXnxq=${term}&endXnxq=${term}`;
-      const result = await ctx.curl(score_url, {
-        method: 'GET',
-        headers: {
-          cookie: `uid=${pass[0].jw_uid}; route=${pass[0].jw_route}`
-        },
-        dataType: 'json'
-      });
-
-      if (result.status === 200) {
-        // 获取成功
-        let data = [];
-        if (result.data.total > 0) {
-          data = await this.processData(user.student_id, term, result.data.results);
-        }
-        ctx.body = {
-          code: 200,
-          message: '成绩列表获取成功',
-          data
-        };
-      } else {
-        // 登录过期，重新登录获取
-        const reauth = await ctx.service.auth.idaas(user.student_id);
-        if (reauth) {
-          // 重新授权成功重新执行
-          await this.index();
-        } else {
-          // 重新授权失败
-          ctx.body = {
-            code: 401,
-            message: '重新授权失败'
-          };
-        }
-      }
-    } catch (err) {
-      // 教务系统无法访问，展示数据库内数据
-      let data;
-      if (term === '001') {
-        // 全学期
-        data = await ctx.app.mysql.select('score', {
-          where: {
-            student_id: user.student_id
-          }
-        });
-      } else {
-        // 指定学期
-        data = await ctx.app.mysql.select('score', {
-          where: {
-            term,
-            student_id: user.student_id
-          }
-        });
-      }
-
+    // Redis Key
+    const cache_key = `score_${user.student_id}_${term}`;
+    // Redis 获取成绩列表
+    const cache = await ctx.app.redis.get(cache_key);
+    if (cache) {
+      // 存在缓存
       ctx.body = {
         code: 201,
         message: '成绩列表获取成功',
-        data
+        data: JSON.parse(cache)
       };
+    } else {
+      // 不存在缓存，从教务系统获取并更新
+      const pass = await ctx.app.mysql.select('user', {
+        where: {
+          student_id: user.student_id
+        }
+      });
+
+      try {
+        // 成绩获取地址
+        const score_base_url = ctx.app.config.jwxt.base + ctx.app.config.jwxt.score;
+        const score_url = `${score_base_url}?page.size=300&page.pn=1&startXnxq=${term}&endXnxq=${term}`;
+        const result = await ctx.curl(score_url, {
+          method: 'GET',
+          headers: {
+            cookie: `uid=${pass[0].jw_uid}; route=${pass[0].jw_route}`
+          },
+          dataType: 'json'
+        });
+
+        if (result.status === 200) {
+          // 获取成功
+          if (result.data.total > 0) {
+            // 有数据
+            const data = await this.processData(user.student_id, term, result.data.results);
+            // 写入 Redis
+            const cache_update = await ctx.app.redis.set(cache_key, JSON.stringify(data), 'EX', 300); // 5 分钟过期
+            if (cache_update === 'OK') {
+              // 更新成功
+              ctx.body = {
+                code: 200,
+                message: '成绩列表获取成功',
+                data
+              };
+            } else {
+              // 更新失败
+              ctx.body = {
+                code: 500,
+                message: '成绩列表缓存更新失败'
+              };
+            }
+          } else {
+            // 无数据
+            ctx.body = {
+              code: 200,
+              message: '成绩列表获取成功',
+              data: []
+            };
+          }
+        } else {
+          // 登录过期，重新登录获取
+          const reauth = await ctx.service.auth.idaas(user.student_id);
+          if (reauth) {
+            // 重新授权成功重新执行
+            await this.index();
+          } else {
+            // 重新授权失败
+            ctx.body = {
+              code: 401,
+              message: '重新授权失败'
+            };
+          }
+        }
+      } catch (err) {
+        // 教务系统无法访问，展示数据库内数据
+        let data;
+        if (term === '001') {
+          // 全学期
+          data = await ctx.app.mysql.select('score', {
+            where: {
+              student_id: user.student_id
+            }
+          });
+        } else {
+          // 指定学期
+          data = await ctx.app.mysql.select('score', {
+            where: {
+              term,
+              student_id: user.student_id
+            }
+          });
+        }
+
+        if (data.length > 0) {
+          // 有数据，写入 Redis
+          const cache_update = await ctx.app.redis.set(cache_key, JSON.stringify(data), 'EX', 300); // 5 分钟过期
+          if (cache_update === 'OK') {
+            // 更新成功
+            ctx.body = {
+              code: 202,
+              message: '成绩列表获取成功',
+              data
+            };
+          } else {
+            // 更新失败
+            ctx.body = {
+              code: 500,
+              message: '成绩列表缓存更新失败'
+            };
+          }
+        } else {
+          // 无数据
+          ctx.body = {
+            code: 202,
+            message: '成绩列表获取成功',
+            data: []
+          };
+        }
+      }
     }
   }
 
@@ -115,6 +167,7 @@ class ScoreListController extends Controller {
     }
     // 对获取到的数据进行处理并插入数据库
     const parse_data = [];
+    const last_update = dayjs().unix();
     for (const item of data) {
       const data = {
         id: item.id.toLowerCase(),
@@ -132,7 +185,8 @@ class ScoreListController extends Controller {
         score: parseInt(item.yscj),
         detail: item.cjfxms,
         term: item.xnxq,
-        student_id
+        student_id,
+        last_update
       };
       // 插入成绩
       await ctx.app.mysql.insert('score', data);
