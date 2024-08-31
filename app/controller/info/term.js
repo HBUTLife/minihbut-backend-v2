94ce53ed036a6e6f43135c2710fb222c44fad115
Souta <soutacn@gmail.com>
2024-08-31 10:44:55 +0800

    fix: 修复部分用户无法获取学期

--- app/controller/info/term.js
+++ app/controller/info/term.js
@@ -11,8 +11,8 @@ class InfoTermController extends Controller {
     // 初始化个人信息
     const user = ctx.user_info;
     // Redis 获取个人学期缓存
-    const cache = await ctx.app.redis.get(`term_${user.student_id}`);
-    if (cache && JSON.parse(cache).length > 0) {
+    const cache = await ctx.app.redis.get(`term_${user.grade_enter}`);
+    if (cache) {
       // 存在缓存
       ctx.body = {
         code: 201,
@@ -21,23 +21,18 @@ class InfoTermController extends Controller {
       };
     } else {
       // 不存在缓存，从数据库中获取
-      const mysql = await ctx.app.mysql.select('term', { orders: [['id', 'desc']] });
-      // 获取成功，根据用户入学年级进行处理
-      const term_list = [];
-      for (const item of mysql) {
-        const start = parseInt(item.name.split('-')[0]);
-        if (start >= user.grade_enter) {
-          term_list.push(item);
-        }
-      }
+      const mysql = await ctx.app.mysql.query(
+        'SELECT name, start_date, start_timestamp, end_date, end_timestamp, total_weeks FROM term WHERE grade_enter >= ? ORDER BY id DESC',
+        [parseInt(user.grade_enter)]
+      );
       // 存入 Redis
-      const cache_update = await ctx.app.redis.set(`term_${user.student_id}`, JSON.stringify(term_list), 'EX', 604800); // 7 天过期
+      const cache_update = await ctx.app.redis.set(`term_${user.grade_enter}`, JSON.stringify(mysql), 'EX', 604800); // 7 天过期
       if (cache_update === 'OK') {
         // 缓存成功
         ctx.body = {
           code: 200,
           message: '学期列表获取成功',
-          data: term_list
+          data: mysql
         };
       } else {
         // 缓存失败
