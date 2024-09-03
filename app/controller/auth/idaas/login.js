6bbfbb2145f3578d6fdb2744b1fd4d94c1aaffa8
Souta <soutacn@gmail.com>
2024-09-03 11:54:42 +0800

    feat: 登录添加返回头像

--- app/controller/auth/idaas/login.js
+++ app/controller/auth/idaas/login.js
@@ -120,7 +120,7 @@ class AuthIdaasLoginController extends Controller {
           code: 202,
           message: '登录成功',
           data: {
-            info,
+            info: { ...info, avatar: local[0].avatar },
             token: this.signToken(info)
           }
         };
@@ -238,7 +238,7 @@ class AuthIdaasLoginController extends Controller {
         delete parse_info.update_time;
         return {
           status: 1,
-          data: parse_info
+          data: { ...parse_info, avatar: hit[0].avatar }
         };
       }
       // 更新失败
@@ -259,7 +259,7 @@ class AuthIdaasLoginController extends Controller {
       delete parse_info.update_time;
       return {
         status: 1,
-        data: parse_info
+        data: { ...parse_info, avatar: null }
       };
     }
     // 插入失败
