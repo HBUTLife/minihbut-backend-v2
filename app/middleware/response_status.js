262da802cd22df2dfbd076609c41cea17ce8ddb1
Souta <soutacn@gmail.com>
2024-09-04 18:08:00 +0800

    fix: 201和202状态码问题

--- app/middleware/response_status.js
+++ app/middleware/response_status.js
@@ -4,7 +4,12 @@ module.exports = () => {
   return async function (ctx, next) {
     await next();
     if (ctx.body && ctx.body.code) {
-      ctx.status = ctx.body.code;
+      const code = ctx.body.code;
+      if (code === 201 || code === 202) {
+        ctx.status = 200;
+      } else {
+        ctx.status = code;
+      }
     }
   };
 };
