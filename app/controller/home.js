57259c7cb76b25e8cefd8a4b4af2f7eefed89e3a
Souta <soutacn@gmail.com>
2024-09-08 21:34:22 +0800

    fix: 修复访问默认页面导致更新全校课表

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,7 +6,6 @@ class HomeController extends Controller {
   async index() {
     const { ctx } = this;
 
-    ctx.app.runSchedule('update_lesson');
     ctx.body = { code: 200, message: 'MiniHBUT API is working' };
   }
 }
