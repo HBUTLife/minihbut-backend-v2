8a87250053fbe3dd2348d8a8f7f0b5c02979bd58
Souta <soutacn@gmail.com>
2024-09-10 21:22:16 +0800

    perf: 优化紧急通知

--- app/controller/info/urgent.js
+++ app/controller/info/urgent.js
@@ -5,32 +5,42 @@ const dayjs = require('dayjs');
 
 class InfoUrgentController extends Controller {
   /**
-   * 获取紧急公告
+   * 获取紧急通知
    */
   async index() {
     const { ctx } = this;
 
-    // 数据库获取
-    const result = await ctx.app.mysql.query(
-      'SELECT title, content FROM urgent WHERE expire_time >= ? ORDER BY id DESC',
-      [dayjs().format('YYYY-MM-DD HH:mm:ss')]
-    );
+    try {
+      // 获取紧急通知
+      const query = await ctx.app.mysql.query(
+        'SELECT title, content FROM urgent WHERE expire_time >= ? ORDER BY id DESC',
+        [dayjs().format('YYYY-MM-DD HH:mm:ss')]
+      );
 
-    // 无紧急通知
-    if (result.length === 0) {
+      // 无紧急通知
+      if (query.length === 0) {
+        ctx.body = {
+          code: 200,
+          message: '无紧急通知'
+        };
+        return;
+      }
+
+      // 有紧急通知
       ctx.body = {
         code: 200,
-        message: '无紧急通知'
+        message: '紧急通知获取成功',
+        data: query[0]
       };
-      return;
-    }
+    } catch (err) {
+      // 数据库查询失败
+      ctx.logger.error(err);
 
-    // 有紧急通知
-    ctx.body = {
-      code: 200,
-      message: '紧急通知获取成功',
-      data: result[0]
-    };
+      ctx.body = {
+        code: 500,
+        message: '服务器内部错误'
+      };
+    }
   }
 }
 
