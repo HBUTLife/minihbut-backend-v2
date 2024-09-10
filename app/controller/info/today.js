8ee7e77b71e2e0bf3afd804f13d4a33132fef245
Souta <soutacn@gmail.com>
2024-09-10 21:20:54 +0800

    perf: 优化今日信息

--- app/controller/info/today.js
+++ app/controller/info/today.js
@@ -9,6 +9,8 @@ class InfoTodayController extends Controller {
    */
   async index() {
     const { ctx } = this;
+
+    // 日期对象
     const today = dayjs();
 
     // Redis Key
@@ -27,50 +29,62 @@ class InfoTodayController extends Controller {
     } else {
       // 不存在缓存
       const today_timestamp = today.unix();
-      const terms = await ctx.app.mysql.query('SELECT * FROM term WHERE start_timestamp <= ? AND end_timestamp >= ?', [
-        today_timestamp,
-        today_timestamp
-      ]);
 
-      let data;
-      if (terms.length > 0) {
-        // 在学期内
-        data = {
-          term: terms[0].name, // 学期
-          total_weeks: terms[0].total_weeks, // 学期总周数
-          info: {
-            date: today.format('YYYY-MM-DD'), // 日期
-            week: Math.ceil((today_timestamp - terms[0].start_timestamp) / (7 * 24 * 3600)), // 周次
-            day: this.getTodayDay(today.day()) // 星期
-          }
-        };
-      } else {
-        // 不在学期内
-        data = {
-          term: '',
-          total_weeks: 24, // 学期总周数，默认为 24
-          info: {
-            date: today.format('YYYY-MM-DD'), // 日期
-            week: 1, // 周次，默认为 1
-            day: this.getTodayDay(today.day()) // 星期
-          }
-        };
-      }
+      try {
+        // 获取学期
+        const query = await ctx.app.mysql.query(
+          'SELECT * FROM term WHERE start_timestamp <= ? AND end_timestamp >= ?',
+          [today_timestamp, today_timestamp]
+        );
 
-      const cache_update = await ctx.app.redis.set(cache_key, JSON.stringify(data), 'EX', 3600); // 1 小时过期
+        let data;
+        if (query.length > 0) {
+          // 在学期内
+          data = {
+            term: query[0].name, // 学期
+            total_weeks: query[0].total_weeks, // 学期总周数
+            info: {
+              date: today.format('YYYY-MM-DD'), // 日期
+              week: Math.ceil((today_timestamp - query[0].start_timestamp) / (7 * 24 * 3600)), // 周次
+              day: this.getTodayDay(today.day()) // 星期
+            }
+          };
+        } else {
+          // 不在学期内
+          data = {
+            total_weeks: 24, // 学期总周数，默认为 24
+            info: {
+              date: today.format('YYYY-MM-DD'), // 日期
+              week: 1, // 周次，默认为 1
+              day: this.getTodayDay(today.day()) // 星期
+            }
+          };
+        }
 
-      if (cache_update === 'OK') {
-        // 更新成功
-        ctx.body = {
-          code: 200,
-          message: '今日信息获取成功',
-          data
-        };
-      } else {
-        // 更新失败
+        // 写入 Redis
+        const cache_update = await ctx.app.redis.set(cache_key, JSON.stringify(data), 'EX', 3600); // 1 小时过期
+
+        if (cache_update === 'OK') {
+          // 更新成功
+          ctx.body = {
+            code: 200,
+            message: '今日信息获取成功',
+            data
+          };
+        } else {
+          // 更新失败
+          ctx.body = {
+            code: 500,
+            message: '服务器内部错误'
+          };
+        }
+      } catch (err) {
+        // 数据库查询失败
+        ctx.logger.error(err);
+
         ctx.body = {
           code: 500,
-          message: '今日信息缓存更新失败'
+          message: '服务器内部错误'
         };
       }
     }
