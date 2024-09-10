f3a8cec85f4bdb52304f0d1b05133148e52ae9af
Souta <soutacn@gmail.com>
2024-09-10 21:09:06 +0800

    perf: 优化学期列表

--- app/controller/info/term.js
+++ app/controller/info/term.js
@@ -8,6 +8,7 @@ class InfoTermController extends Controller {
    */
   async index() {
     const { ctx } = this;
+
     // 初始化个人信息
     const user = ctx.user_info;
 
@@ -22,32 +23,43 @@ class InfoTermController extends Controller {
         data: JSON.parse(cache)
       };
     } else {
-      // 不存在缓存，从数据库中获取
-      const mysql = await ctx.app.mysql.query(
-        'SELECT name, start_date, start_timestamp, end_date, end_timestamp, total_weeks FROM term WHERE grade_enter >= ? ORDER BY id DESC',
-        [parseInt(user.grade_enter)]
-      );
+      // 不存在缓存
+      try {
+        // 获取学期列表
+        const query = await ctx.app.mysql.query(
+          'SELECT name, start_date, start_timestamp, end_date, end_timestamp, total_weeks FROM term WHERE grade_enter >= ? ORDER BY id DESC',
+          [parseInt(user.grade_enter)]
+        );
 
-      // 存入 Redis
-      const cache_update = await ctx.app.redis.set(
-        `info_term_${user.grade_enter}`,
-        JSON.stringify(mysql),
-        'EX',
-        604800
-      ); // 7 天过期
+        // 存入 Redis
+        const cache_update = await ctx.app.redis.set(
+          `info_term_${user.grade_enter}`,
+          JSON.stringify(query),
+          'EX',
+          604800
+        ); // 7 天过期
 
-      if (cache_update === 'OK') {
-        // 缓存成功
-        ctx.body = {
-          code: 200,
-          message: '学期列表获取成功',
-          data: mysql
-        };
-      } else {
-        // 缓存失败
+        if (cache_update === 'OK') {
+          // 缓存成功
+          ctx.body = {
+            code: 200,
+            message: '学期列表获取成功',
+            data: query
+          };
+        } else {
+          // 缓存失败
+          ctx.body = {
+            code: 500,
+            message: '服务器内部错误'
+          };
+        }
+      } catch (err) {
+        // 数据库查询失败
+        ctx.logger.error(err);
+
         ctx.body = {
           code: 500,
-          message: '学期列表缓存失败'
+          message: '服务器内部错误'
         };
       }
     }
